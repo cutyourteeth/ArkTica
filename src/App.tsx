322e0dee14de0e259729dc7b52ffffedc8fde033
caper <qq174028@sina.com>
2019-10-31 21:45:26 +0800

    'auto-commit'

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,23 @@ const App: React.FC = () => {
   console.log(AppStore)
 
   return (
-    // <AppStore.Provider>
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/" exact={true}>
-            <Home />
-          </Route>
-          <Route path="/editor" exact={true}>
-            <Editor />
-          </Route>
-          <Route path="/reader" exact={true}>
-            <Reader />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-    // </AppStore.Provider>
+    <AppStore.Provider>
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Home />
+            </Route>
+            <Route path="/editor" exact={true}>
+              <Editor />
+            </Route>
+            <Route path="/reader" exact={true}>
+              <Reader />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </AppStore.Provider>
   )
 }
 
